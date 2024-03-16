58bc1a3f3a19988a45efd0c5a5b0d12ad0d11645
theunrealhulk <chafiaay.abj@gmail.com>
2024-03-16 15:42:09 +0000

    added cors for angular front

--- Program.cs
+++ Program.cs
@@ -38,9 +38,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuer = false,
             ValidateAudience = false
         };
-    }); 
-   
-   
+    });
+
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins", policy =>
+{
+    policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,6 +52,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("NgOrigins");
 
 app.UseHttpsRedirection();
 
